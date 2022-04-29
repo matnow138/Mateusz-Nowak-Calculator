632f37f1244014fd8e47278525430dbaaf82d531
Mateusz Nowak <mateuszp.nowak@gmail.com>
2022-04-29 11:19:26 +0200

    First Commit

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -1,2 +1,24 @@
-package com.kodilla.calculator;public class Calculator {
-}
+package com.kodilla.calculator;
+
+public class Calculator {
+
+
+
+        int a;
+        int b;
+        public void add(int a, int b){
+            System.out.println(a+b);
+        }
+        public void min(int a, int b){
+            System.out.println(a-b);
+        }
+
+        public static void main(String args[]) {
+            Calculator calculator = new Calculator();
+            calculator.add(2,3);
+            calculator.min(2,3);
+
+        }
+    }
+
+
