f531756e67749d5663323080217889e9a565b6a6
Mateusz Nowak <mateuszp.nowak@gmail.com>
2022-05-03 22:20:34 +0200

    ok

--- src/main/java/com/kodilla/calculator/Calculator.java
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
             System.out.println(a+b);
         }
         public void min(int a, int b){
-            System.out.println(a-b);
+            "Result: " + System.out.println(a-b);
         }
 
         public static void main(String args[]) {
